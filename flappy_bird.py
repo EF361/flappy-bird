c434c712c1656019460e88d58a5e9e6c38b4b89a
EF361 <aliciagoh0704@gmail.com>
2024-09-19 14:45:05 +0800

    initial commit

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,9 +1,24 @@
 import pygame
 
 pygame.init()
-screen_width = 864
-screen_height = 936
+screen_width = 500
+screen_height = 650
 
 # set the screen size and title
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird")
+
+# load images
+bg = pygame.image.load("img/bg.png")
+
+run = True
+while run:
+    # show images
+    screen.blit(bg, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
