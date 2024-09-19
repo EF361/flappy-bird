f576f99e9241344035a2526af4e313d8d6358418
EF361 <aliciagoh0704@gmail.com>
2024-09-19 15:09:14 +0800

    add scroll background

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,20 +1,39 @@
 import pygame
 
 pygame.init()
-screen_width = 500
-screen_height = 650
+
+clock = pygame.time.Clock()
+fps = 60
+
+
+screen_width = 648
+screen_height = 702
 
 # set the screen size and title
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird")
 
+# define game variables
+ground_scroll = 0
+scroll_speed = 4
+
 # load images
 bg = pygame.image.load("img/bg.png")
+ground_img = pygame.image.load("img/ground.png")
 
 run = True
 while run:
-    # show images
-    screen.blit(bg, (0, 0))
+    clock.tick(fps)
+
+    # draw background
+    screen.blit(bg, (0, -100))
+
+    # draw and scroll the ground
+    screen.blit(ground_img, (ground_scroll, 576))
+    ground_scroll -= scroll_speed
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
