807457758d015d6cfa4479337ccadb1d57f469c5
EF361 <aliciagoh0704@gmail.com>
2024-09-19 14:31:29 +0800

    intial commit

---
+++ flappy_bird.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+screen_width = 864
+screen_height = 936
+
+# set the screen size and title
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Flappy Bird")
