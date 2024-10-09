d48d4221b030baa1fba97861fcaec049c4287606
EF361 <aliciagoh0704@gmail.com>
2024-10-09 15:03:22 +0800

    update main.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,5 +1,10 @@
 import pygame
+from pygame.locals import *  # noqa: F403
+import random
+
+# import self define game modules
 from bird import Bird
+from pipe import Pipe
 
 pygame.init()
 
@@ -18,6 +23,9 @@ ground_scroll = 0
 scroll_speed = 4
 flying = False
 game_over = False
+pipe_gap = 150
+pipe_frequency = 1500  # milliseconds
+last_pipe = pygame.time.get_ticks() - pipe_frequency
 
 # load images
 bg = pygame.image.load("img/bg.png")
@@ -25,10 +33,13 @@ ground_img = pygame.image.load("img/ground.png")
 
 
 bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
+
 
 flappy = Bird(100, int(screen_height / 2))
 
 bird_group.add(flappy)
+
 
 run = True
 while run:
@@ -39,20 +50,40 @@ while run:
 
     bird_group.draw(screen)
     bird_group.update(flying, game_over)
+    pipe_group.draw(screen)
 
     # draw the ground
     screen.blit(ground_img, (ground_scroll, 576))
 
+    # look for collision
+    if (
+        pygame.sprite.groupcollide(bird_group, pipe_group, False, False)
+        or flappy.rect.top < 0
+    ):
+        game_over = True
+
     # check if bird has hit the ground
-    if flappy.rect.bottom > 576:
+    if flappy.rect.bottom >= 576:
         game_over = True
         flying = False
 
-    if not game_over:
+    if not game_over and flying:
+        # generate new pipes
+        time_now = pygame.time.get_ticks()
+        if time_now - last_pipe > pipe_frequency:
+            pipe_height = random.randint(-100, 100)
+            btm_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, -1)
+            top_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, 1)
+            pipe_group.add(btm_pipe)
+            pipe_group.add(top_pipe)
+            last_pipe = time_now
+
         # draw and scroll the ground
         ground_scroll -= scroll_speed
         if abs(ground_scroll) > 35:
             ground_scroll = 0
+
+        pipe_group.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
