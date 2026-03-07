a4d4f223b3aad17a4af291fa562ca74b28fb4186
EF361 <aliciagoh0704@gmail.com>
2026-03-07 15:18:33 +0800

    modify the pipe

--- pipe.py
+++ pipe.py
@@ -1,22 +1,21 @@
 import pygame
-
-pipe_gap = 150
-scroll_speed = 4
-
+from config import Config
 
 class Pipe(pygame.sprite.Sprite):
     def __init__(self, x, y, position):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("img/pipe.png")
         self.rect = self.image.get_rect()
-        # position 1 is from the top, -1 is from the bottom
+        
+        gap = 160 if Config.hard_mode else 200
+        
         if position == 1:
             self.image = pygame.transform.flip(self.image, False, True)
-            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+            self.rect.bottomleft = [x, y - int(gap / 2)]
         if position == -1:
-            self.rect.topleft = [x, y + int(pipe_gap / 2)]
+            self.rect.topleft = [x, y + int(gap / 2)]
 
-    def update(self):
-        self.rect.x -= scroll_speed
+    def update(self, speed):
+        self.rect.x -= speed
         if self.rect.right < 0:
             self.kill()
